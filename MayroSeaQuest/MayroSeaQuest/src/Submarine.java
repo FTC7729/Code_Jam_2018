78684609bc356149f7bc32147b3ad5d0ab617d8e
kaladharp <srimaye.peddinti.576@gmail.com>
2018-05-03 19:25:32 -0400

    i made the submarine

--- MayroSeaQuest/MayroSeaQuest/src/Submarine.java
+++ MayroSeaQuest/MayroSeaQuest/src/Submarine.java
@@ -3,9 +3,16 @@ public class Submarine extends Sprite {
 
 	@Override
 	char[][] render(char[][] charSprite, int iX, int iY) {
-		// TODO Auto-generated method stub
+		
+		charSprite  [iX+1][iY+1] = '/'; charSprite [iX+2][iY+1] = '+'; 
+		charSprite [iX+3][iY+1] = '_'; charSprite [iX+4][iY+1] = '_'; 
+		charSprite [iX + 1][iY+2] = '|'; charSprite [iX+4][iY+2] = 'o';
+		charSprite [iX +5][iY+2] = '\\'; charSprite [iX + 1][iY+3] = '\\';
+		charSprite [iX + 1][iY+3] = '_'; charSprite [iX][iY+2] = '+';
+		charSprite [iX+3][iY+3] = '_'; charSprite [iX+4][iY+3] = '+';
+		charSprite [iX+5][iY+3] = '/';
 
-		return null;
+		return charSprite;
 	}
 	
 
