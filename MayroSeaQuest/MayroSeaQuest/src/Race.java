3da7645c8a767d793ac3efd7afb0542023b05fe0
jminto <jminto@BLADE>
2018-05-05 08:35:45 -0400

    Added key press routines

--- MayroSeaQuest/MayroSeaQuest/src/Race.java
+++ MayroSeaQuest/MayroSeaQuest/src/Race.java
@@ -12,9 +12,15 @@ public class Race {
 		DOWN
 	}
 	
-	protected Scanner myScanner;
+	protected Scanner	myScanner;
 	protected STATE_HORIZONTAL	enumHorizontal;
 	protected STATE_VERTICAL	enumVertical;
+	protected Submarine	mySubmarine;
+	
+	public Race() {
+		mySubmarine = new Submarine();
+		mySubmarine.init(0,12);
+	}
 	
 	public void init(Scanner myScanner) {
 		this.myScanner = myScanner;
@@ -29,30 +35,53 @@ public class Race {
 		
 		while ( bLoop ) {
 			GetInput();
+			UpdateSubPosition();
 			
 		} // while bLoop
 	} // Race Loop
 	
 	protected void GetInput() {
-		String	strDirection = this.myScanner.next();
-		switch( strDirection ) {
-		case "W":
-			// Move up
-			this.enumVertical = STATE_VERTICAL.UP;
-			break;
-		case "A":
-			// Move left
-			this.enumHorizontal = STATE_HORIZONTAL.LEFT;
-			break;
-		case "D":
-			// Move right
-			this.enumHorizontal = STATE_HORIZONTAL.RIGHT;
-			break;
-		case "S":
-			// Move down
-			this.enumVertical = STATE_VERTICAL.DOWN;
-			break;
-		} // switch keypress
+		while (this.myScanner.hasNext()) {
+			String	strDirection = this.myScanner.next();
+			switch( strDirection ) {
+			case "W":
+				// Move up
+				this.enumVertical = STATE_VERTICAL.UP;
+				break;
+			case "A":
+				// Move left
+				this.enumHorizontal = STATE_HORIZONTAL.LEFT;
+				break;
+			case "D":
+				// Move right
+				this.enumHorizontal = STATE_HORIZONTAL.RIGHT;
+				break;
+			case "S":
+				// Move down
+				this.enumVertical = STATE_VERTICAL.DOWN;
+				break;
+			} // switch keypress
+		} // while hasnext
 	} // GetInput
+	
+	protected void UpdateSubPosition() {
+		switch (this.enumVertical){
+			case UP:
+				this.mySubmarine.iY++;
+				break;
+			case DOWN:
+				this.mySubmarine.iY--;
+				break;
+		} // Vertical movement
+		switch (this.enumHorizontal) {
+			case LEFT:
+				this.mySubmarine.iX--;
+				break;
+			case RIGHT:
+				this.mySubmarine.iX++;
+				break;
+		}// Horizontal movement
+				
+	} // UpdateSubPosition
 	
 }
--- MayroSeaQuest/MayroSeaQuest/src/Sprite.java
+++ MayroSeaQuest/MayroSeaQuest/src/Sprite.java
@@ -1,5 +1,23 @@
 
 public abstract class Sprite {
+	
+	int	iX, iY; // location of sprite
+	
+	public Sprite() {
+		iX = 0;
+		iY = 0;
+	}
+	
+	public Sprite(int iStartX, int iStartY) {
+		this.init(iStartX, iStartY);
+	}
+	
+	public boolean init(int iStartX, int iStartY) {
+		iX = iStartX;
+		iY = iStartY;
+		
+		return true;
+	}
 
 	abstract char[][] render(char[][] charSprite, int iX, int iY);
 
